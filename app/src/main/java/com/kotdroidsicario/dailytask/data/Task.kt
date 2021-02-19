6b3565738e9a2e8b2e0920bab5df8383dc9f9406
paul <paulmburu53@gmail.com>
2021-02-19 06:01:03 +0300

    update Task

--- app/src/main/java/com/kotdroidsicario/dailytask/data/Task.kt
+++ app/src/main/java/com/kotdroidsicario/dailytask/data/Task.kt
@@ -2,27 +2,23 @@ package com.kotdroidsicario.dailytask.data
 
 import android.os.Parcelable
 import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.kotdroidsicario.dailytask.util.validateTime
 import kotlinx.android.parcel.Parcelize
+import java.util.*
 
 @Parcelize
 @Entity(tableName = "tasks")
 data class Task(
-    @PrimaryKey(autoGenerate = true)
-    @NonNull
-    val id: Long = 0L,
-    @NonNull
-    val title: String,
-    @NonNull
-    val description: String,
-    @NonNull
-    val hours: Int,
-    @NonNull
-    val minutes:Int,
-    @NonNull
-    val isComplete: Boolean = false
+    @PrimaryKey()
+    @ColumnInfo(name = "entryid") var id: String = UUID.randomUUID().toString(),
+    @ColumnInfo(name = "title") val title: String,
+    @ColumnInfo(name = "description") val description: String,
+    @ColumnInfo(name = "hours") val hours: Int,
+    @ColumnInfo(name = "minutes") val minutes:Int,
+    @ColumnInfo(name = "completed") val isComplete: Boolean = false
 ) : Parcelable {
     val time: String
         get() = "${hours.validateTime()}:${minutes.validateTime()}"
